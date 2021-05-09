5ee77a21ee67162eecd742009fdf38ba89332a88
Daniel Michael <itzdanielmichael@gmail.com>
2021-05-08 20:23:31 -0400

    Create toPascalCase.js

---
+++ toPascalCase.js
@@ -0,0 +1,13 @@
+function toPascalCase(string) {
+  return `${string}`
+    .replace(new RegExp(/[-_]+/, 'g'), ' ')
+    .replace(new RegExp(/[^\w\s]/, 'g'), '')
+    .replace(
+      new RegExp(/\s+(.)(\w+)/, 'g'),
+      ($1, $2, $3) => `${$2.toUpperCase() + $3.toLowerCase()}`
+    )
+    .replace(new RegExp(/\s/, 'g'), '')
+    .replace(new RegExp(/\w/), s => s.toUpperCase());
+}
+
+// https://stackoverflow.com/questions/4068573/convert-string-to-pascal-case-aka-uppercamelcase-in-javascript
